8036e0f1d689dacd9ce60f3bb586410183f7c024
Juan Hidalgo <juanchidalgo94@gmail.com>
2023-04-02 15:52:12 -0500

    Final changes for the project

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ class Country {
 // Creamos los paises destino: 
 
 
-    const Argentina = new Country(1, 'Argentina', 'Buenos Aires', 'Ministro Pistarini International Airport', 7, ' Argentine Pesos', 500, 23)
-    const Brazil =  new Country(2, 'Brazil', 'São Paulo', 'São Paulo-Guarulhos International Airport', 5, 'Brazilian Real', 400, 25)
-    const Chile = new Country(3, 'Chile', 'Santiago de Chile', 'Santiago International Airport', 6, 'Chilean Pesos', 440, 45)
-    const Mexico = new Country(4, 'Mexico', 'Cancun', 'Cancún International Airport', 4, 'Mexican Pesos', 350, 30)
-    const Dominican = new Country(5, 'Dominican Republic', 'Punta Cana', 'Punta Cana International Airport', 4, 'Dominican Pesos', 380, 35)
+const Argentina = new Country(1, 'Argentina', 'Buenos Aires', 'Ministro Pistarini International Airport', 7, ' Argentine Pesos', 500, 23)
+const Brazil = new Country(2, 'Brazil', 'São Paulo', 'São Paulo-Guarulhos International Airport', 5, 'Brazilian Real', 400, 25)
+const Chile = new Country(3, 'Chile', 'Santiago de Chile', 'Santiago International Airport', 6, 'Chilean Pesos', 440, 45)
+const Mexico = new Country(4, 'Mexico', 'Cancun', 'Cancún International Airport', 4, 'Mexican Pesos', 350, 30)
+const Dominican = new Country(5, 'Dominican Republic', 'Punta Cana', 'Punta Cana International Airport', 4, 'Dominican Pesos', 380, 35)
 
 // Guardamos los paises
 const countries = [Argentina, Brazil, Chile, Mexico, Dominican]
@@ -31,17 +31,17 @@ const countries = [Argentina, Brazil, Chile, Mexico, Dominican]
 // console.log(countries)
 
 const selectNodeDestination = document.querySelector('#destination')
-countries.forEach((p)=> {
+countries.forEach((p) => {
     const optionDest = document.createElement('option')
     optionDest.innerText = `${p.destination}`  // , ${p.country}
-    optionDest.setAttribute('id',p.id)
+    optionDest.setAttribute('id', p.id)
     selectNodeDestination.append(optionDest)
 })
 
 // Creando las season
 
 class Season {
-    constructor(id,called,value) {
+    constructor(id, called, value) {
         this.id = id
         this.called = called
         this.value = value
@@ -49,33 +49,32 @@ class Season {
 }
 
 // Creando las season
-const Low = new Season(1,'Low Season', 1)
-const Shoulder = new Season(2,'Shoulder Season', 1.5)
-const High = new Season(3,'High Season', 2)
+const Low = new Season(1, 'Low Season', 1)
+const Shoulder = new Season(2, 'Shoulder Season', 1.5)
+const High = new Season(3, 'High Season', 2)
 
-    // Guardamos las season
-    const seasons = [Low, Shoulder, High]
+// Guardamos las season
+const seasons = [Low, Shoulder, High]
 
-    const selectNodeSeason = document.querySelector('#season')
-    seasons.forEach((p)=> {
-        const optionSeas = document.createElement('option')
-        optionSeas.innerText = `${p.called}`
-        optionSeas.setAttribute('id',p.id)
-        selectNodeSeason.append(optionSeas)
-    })
+const selectNodeSeason = document.querySelector('#season')
+seasons.forEach((p) => {
+    const optionSeas = document.createElement('option')
+    optionSeas.innerText = `${p.called}`
+    optionSeas.setAttribute('id', p.id)
+    selectNodeSeason.append(optionSeas)
+})
 
-    // Tomando los valores del form: 
+// Tomando los valores del form: 
 
 const form0 = document.getElementById('form0')
 const inputName = document.getElementById('full_name')
 const inputEmail = document.getElementById('email_address')
 const inputDestination = document.getElementById('destination')
 const inputSeason = document.getElementById('season')
-const inputTravelers= document.getElementById('travelers')
-const inputDays= document.getElementById('days')
+const inputTravelers = document.getElementById('travelers')
+const inputDays = document.getElementById('days')
 const inputDate = document.getElementById('date')
 
-// console.log(inputEmail);
 
 // Click sobre el botón Submit. 
 
@@ -93,15 +92,15 @@ form0.onsubmit = (e) => {
 
     const infoUserJSON = JSON.stringify(infoUser);
     console.log(infoUserJSON);
-    localStorage.setItem('infoUser',infoUserJSON);
-    
+    localStorage.setItem('infoUser', infoUserJSON);
+
 }
 
 
 
 // Botón para limpiar el localstorage. 
 const addButtonClear = document.querySelector('#clear')
-addButtonClear.addEventListener('click', function(){
+addButtonClear.addEventListener('click', function () {
     localStorage.clear();
     location.reload();
 })
@@ -127,9 +126,7 @@ function calcu(n1, n2, fn) {
     return result
 }
 
-// Uso de Function 
-
-
+// retomar info del storage
 
 const infoUser0 = localStorage.getItem('infoUser')
 console.log(infoUser0);
@@ -138,43 +135,40 @@ console.log(infoUser1);
 
 console.log(infoUser1.season);
 
-    const country0 = countries.find(c => {
-        return c.destination === infoUser1.destination
-    })
+// reconocer el country and the season based on the answer. 
+const country0 = countries.find(c => {
+    return c.destination === infoUser1.destination
+})
 
-    console.log(country0);
-   
-    const season0 = seasons.find(s => {
-        return s.called === infoUser1.season
-    })
+console.log(country0);
 
-    console.log(season0);
+const season0 = seasons.find(s => {
+    return s.called === infoUser1.season
+})
 
-const finalFlight = calcu(calcu(country0.flight,season0.value,mult),infoUser1.travelers,mult)
-const finalBudget = calcu(calcu(calcu(country0.budget,season0.value,mult),infoUser1.travelers,mult),infoUser1.days,mult)
-const finalMoney = calcu(finalBudget,finalFlight,sum)
+console.log(season0);
+
+// Calculos para el presupuesto del viaje
+const finalFlight = calcu(calcu(country0.flight, season0.value, mult), infoUser1.travelers, mult)
+const finalBudget = calcu(calcu(calcu(country0.budget, season0.value, mult), infoUser1.travelers, mult), infoUser1.days, mult)
+const finalMoney = calcu(finalBudget, finalFlight, sum)
 
 console.log(finalBudget, finalFlight, finalMoney)
 
 
-// const finalFlight0 = calcu(calcu(country[]))
-
-// console.log(finalFlight0);
-
-
-// Mirar si en storage existe infoUser
+// Modificar el HTML para respuesta final.
 
 let final_answer = document.getElementById("final_answer");
 
 const infoUser = localStorage.getItem('infoUser')
 const infoUserJS = JSON.parse(infoUser)
-if(infoUser){
+if (infoUser) {
     form0.remove()
     finale.remove()
     finale1.remove()
-    final_answer.innerHTML = `Thank you for choosing us, ${infoUser1.name}. Next, you will find the final budget review.
+    final_answer.innerHTML = `Thank you for choosing us, ${infoUser1.name}. Next, you will find the final budget review. <br>
 
     To stay ${infoUser1.days} days on ${country0.destination}, ${country0.country} during ${season0.called} for ${infoUser1.travelers} travelers, we recommend a budget of USD ${finalBudget} to cover accommodation, food, and transportation expenses. Also, we estimate that you may need USD ${finalFlight} approx for flight tickets. The total budget is USD ${finalMoney}.
-    
-    Moreover, here is some information to have in mind for your trip to ${country0.destination}, ${country0.country}: The estimated time of flight is ${country0.hours} hours, you will arrive to ${country0.airport} and the local currency will be ${country0.currency}.`; 
- }
+    <br> 
+    Moreover, here is some information to have in mind for your trip to ${country0.destination}, ${country0.country}: The estimated time of flight is ${country0.hours} hours, you will arrive to ${country0.airport} and the local currency will be ${country0.currency}.`;
+}
